82e698d8247f14a93877ab200fc8a0cb5e0861e6
Martti Kesaniemi <martti.kesaniemi@nokia.com>
2011-01-05 16:39:43 +0200

    Added overloaded constructor to avoid binary break

--- src/quillmetadata.cpp
+++ src/quillmetadata.cpp
@@ -69,6 +69,23 @@ QuillMetadata::QuillMetadata()
 }
 
 QuillMetadata::QuillMetadata(const QString &fileName,
+                             MetadataFormatFlags formats)
+{
+    init();
+    priv = new QuillMetadataPrivate;
+    if (formats == ExifFormat) {
+        priv->xmp = new Xmp();
+        priv->isXmpNeeded = false;
+    }
+    else {
+        priv->xmp = new Xmp(fileName);
+        priv->isXmpNeeded = true;
+    }
+    priv->exif = new Exif(fileName);
+}
+
+
+QuillMetadata::QuillMetadata(const QString &fileName,
                              MetadataFormatFlags formats,
                              Tag tagToRead)
 {
